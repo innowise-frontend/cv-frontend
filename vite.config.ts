344fb5caa25b6a5a64bb6d01fd12c037a6b21794
Maryia Kunts <marykunc@gmail.com>
2026-03-31 10:21:15 +0200

    2-fe: update  vite.config to pass tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
@@ -26,5 +26,10 @@ export default defineConfig({
       "@styles": path.resolve(__dirname, "src/styles"),
       "@hooks": path.resolve(__dirname, "src/hooks"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.ts"],
+    globals: true,
   },
 });
