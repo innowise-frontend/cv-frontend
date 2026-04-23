6252e04a5997bf3c4b7c9a37104214f215dba8f3
Ustina Korbut <ustsiniya.korbut@innowise.com>
2026-04-23 16:43:33 +0200

    Revert "fix: fix test"
    This reverts commit 1fdf32ab011a2f2def1f67a25849185bf4916076.

--- src/pages/SettingPage/SettingPage.test.tsx
+++ src/pages/SettingPage/SettingPage.test.tsx
@@ -27,6 +27,7 @@ vi.mock("@components/shared/Select/Select", () => ({
   Select: ({
     label,
     value,
+    multiple,
     onValueChange,
   }: {
     label: string;
@@ -42,6 +43,7 @@ vi.mock("@components/shared/Select/Select", () => ({
         onClick={() => {
           if (label === "page.setting.theme") onValueChange("dark");
           if (label === "page.setting.language") onValueChange("uk");
+          if (multiple) onValueChange(["react", "ts"]);
         }}
       >
         change-{label}
@@ -72,6 +74,7 @@ describe("SettingPage", () => {
 
     expect(screen.getByText("page.setting.theme")).toBeInTheDocument();
     expect(screen.getByText("page.setting.language")).toBeInTheDocument();
+    expect(screen.getByText("Multi-select example")).toBeInTheDocument();
     expect(screen.getByTestId("change-password")).toBeInTheDocument();
   });
 
@@ -90,5 +93,13 @@ describe("SettingPage", () => {
     fireEvent.click(screen.getByRole("button", { name: "change-page.setting.language" }));
 
     expect(changeLanguageMock).toHaveBeenCalledWith("uk");
+  });
+
+  it("updates multi-select value", () => {
+    render(<SettingPage />);
+
+    expect(screen.getByTestId("Multi-select example-value")).toBeEmptyDOMElement();
+    fireEvent.click(screen.getByRole("button", { name: "change-Multi-select example" }));
+    expect(screen.getByTestId("Multi-select example-value")).toHaveTextContent("react,ts");
   });
 });
