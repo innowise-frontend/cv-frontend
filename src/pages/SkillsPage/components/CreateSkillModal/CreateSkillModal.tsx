7af6e535981d57304b34b3fa0aca319986be7fde
Ustina Korbut <ustsiniya.korbut@innowise.com>
2026-06-11 10:20:54 +0200

    bug(150): fix color for button

--- src/pages/SkillsPage/components/CreateSkillModal/CreateSkillModal.tsx
+++ src/pages/SkillsPage/components/CreateSkillModal/CreateSkillModal.tsx
@@ -35,7 +35,7 @@ export const CreateSkillModal = () => {
 
   return (
     <>
-      <Modal.Trigger className="text-red font-medium">
+      <Modal.Trigger className="text-red font-medium dark:text-red">
         <PlusIcon height={16} width={16} />
         {t("page.skills.createSkill")}
       </Modal.Trigger>
